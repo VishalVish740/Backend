cd926eddee084ae82bab59a4d838ef8e58d5cf18
VishalVish740 <vishal72760@gmail.com>
2025-10-23 22:13:21 +0530

    fix

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const todoRoutes = require('./routes/todo');
 const errorHandler = require('./middleware/errorHandler');
 
 // Middleware
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://todo-frontend-tau-blush.vercel.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}))
+}));
+
 
 app.use(express.json());
 
